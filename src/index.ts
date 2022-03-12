9d22d804e5dbadbb76bebfc87e8ac4aa41e53213
김지웅 <godjw96@gmail.com>
2022-03-12 19:56:07 +0900

    Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-const name = "Kim",
-    age = 24,
-    gender = "male";
+interface Human{
+    name: string;
+    age: number;
+    gender: string;
+}
 
-const sayHi = (name: string, age:number, gender: string): void => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+const person = {
+    name: "kim",
+    age: 24,
+    gender: "male"
+}
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
 };
 
-sayHi("kim:", 24, "male");
+console.log(sayHi(person));
 
 export {};
