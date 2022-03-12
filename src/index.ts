c590c5f5a46cede59e966b51742382e5e2bf1c9c
김지웅 <godjw96@gmail.com>
2022-03-12 23:39:36 +0900

    Validate Block and Add Block

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,14 @@ class Block {
         timestamp: number,
         data: string
     ) : string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    static validateStructure = (aBlock : Block) : boolean => 
+        typeof aBlock.index === "number" &&
+        typeof aBlock.hash === "string" &&
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string";
+    
 }
 
 const genesisBlock:Block = new Block(0, "2020202020202", "", "Hello", 123456);
@@ -54,11 +62,43 @@ const createNewBlock = (data:string): Block => {
         data,
         newTimestamp
     );
+    addBlock(newBlock);
     return newBlock;
 };
 
-console.log(createNewBlock("hello"));
-console.log(createNewBlock("bye bye"))
+const getHashforBlock = (aBlock: Block): string => 
+    Block.calculateBlockHash(
+        aBlock.index,
+        aBlock.previousHash,
+        aBlock.timestamp,
+        aBlock.data
+    );
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+    if(!Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false
+    }else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
+};
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 
 
